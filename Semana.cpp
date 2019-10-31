4473ec4fe849763b14486ea285191189d5812874
DOUGLAS FAGUNDES KLEIN <12205173@pucrs.br>
2019-10-31 19:05:39 -0300

    inicial

---
+++ Semana.cpp
@@ -0,0 +1,100 @@
+#include <iostream.h>
+#include <fstream.h>
+#include <string.h>
+
+class Strategy;
+
+
+class StrategySeg
+{
+  public:
+    ImprimeSeg()
+    {
+
+    }
+
+  private:
+};
+
+
+class StrategyTer
+{
+  public:
+    ImprimeTer()
+    {
+
+    }
+
+  private:
+};
+
+
+class StrategyQua
+{
+  public:
+    ImprimeQua()
+    {
+
+    }
+
+  private:
+};
+
+
+class StrategyQui
+{
+  public:
+    ImprimeQui()
+    {
+
+    }
+
+  private:
+};
+
+
+class StrategySex
+{
+  public:
+    ImprimeSex()
+    {
+
+    }
+
+  private:
+};
+
+
+class StrategySab
+{
+  public:
+    ImprimeSab()
+    {
+
+    }
+
+  private:
+};
+
+
+class StrategyDom
+{
+  public:
+    ImprimeDom()
+    {
+
+    }
+
+  private:
+};
+
+
+int main()
+{
+  Strategy semana // classe "Strategy" chama objeto "semana"
+
+  //make your code here...
+  return 0;
+}
+
+
